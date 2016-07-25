c4084d969c413769b4dabe99afbb05becab95fe3
Jon Shier <jon@jonshier.com>
2016-07-24 22:03:28 -0400

    Migrate to Swift 3 beta 3.

--- PKHUD/PKHUDAnimation.swift
+++ PKHUD/PKHUDAnimation.swift
@@ -14,39 +14,11 @@ public final class PKHUDAnimation {
     
     static let discreteRotation: CAAnimation = {
         let animation = CAKeyframeAnimation(keyPath: "transform.rotation.z")
-        animation.values = [
-            NSNumber(float: 0.0),
-            NSNumber(float: 1.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 2.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 3.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 4.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 5.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 6.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 7.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 8.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 9.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 10.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 11.0 * Float(M_PI) / 6.0),
-            NSNumber(float: 2.0 * Float(M_PI))
-        ]
-        animation.keyTimes = [
-            NSNumber(float: 0.0),
-            NSNumber(float: 1.0 / 12.0),
-            NSNumber(float: 2.0 / 12.0),
-            NSNumber(float: 3.0 / 12.0),
-            NSNumber(float: 4.0 / 12.0),
-            NSNumber(float: 5.0 / 12.0),
-            NSNumber(float: 0.5),
-            NSNumber(float: 7.0 / 12.0),
-            NSNumber(float: 8.0 / 12.0),
-            NSNumber(float: 9.0 / 12.0),
-            NSNumber(float: 10.0 / 12.0),
-            NSNumber(float: 11.0 / 12.0),
-            NSNumber(float: 1.0)
-        ]
+        animation.values = (0...12).map { NSNumber(value: Double(Double($0) * Double.pi / 6.0)) }
+        animation.keyTimes = (0...12).map { NSNumber(value: Double(Double($0) / 12.0)) }
         animation.duration = 1.2
         animation.calculationMode = "discrete"
-        animation.repeatCount = Float(INT_MAX)
+        animation.repeatCount = .infinity
         return animation
     }()
 
@@ -54,9 +26,9 @@ public final class PKHUDAnimation {
     static let continuousRotation: CAAnimation = {
         let animation = CABasicAnimation(keyPath: "transform.rotation.z")
         animation.fromValue = 0
-        animation.toValue = 2.0 * M_PI
+        animation.toValue = 2.0 * .pi
         animation.duration = 1.2
-        animation.repeatCount = Float(INT_MAX)
+        animation.repeatCount = .infinity
         return animation
     }()
 }
